b25b1e44544bb5893d879fba2fbeceda2f423ff5
chriskelsey <chriskelsey.net@gmail.com>
2018-06-03 11:43:12 -0700

    Updated html and js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,41 @@
 // Declare all the universal variables
 var movies = ["alien", "aliens", "blade runner", "metropolis", "arrival", "the martian", "star wars", "sunshine", "star trek", "predestination", "serenity"];
 var random = Math.floor(Math.random() * movies.length);
-var randMov = movies[random];
+var randMov = movies[random].split("");
 var remaining = 10;
+var replacer = [];
 var score = 0;
 var abc = /([a-z])/;//Regular expression, so it can be only letters
 
 $(document).ready(function() {
-	var filler = "";
+	console.log(randMov);
 	for (var i = 0; i < randMov.length; i++) {
-		if (randMov.charAt(i) === " ") {
-			filler = filler + " ";
+		if (randMov[i] === " ") {
+			$('.clues').append(" ");
+			replacer.push(" ")
 		} else {
-			filler = filler + "_";
+			$('.clues').append("_");
+			replacer.push("_")
 		}
 	}
-	$('.clues').text(filler);
-	$(document).on('keyup', function(evt){letterComp(evt.key)});
+	console.log(replacer);
+	
+	$(document).on('keyup', function(evt){letterComp(randMov,replacer,evt.key)});
 });
 
-function letterComp(str) {
-	console.log(str);
-	var replacer = "";
-	for (var i = 0; i < randMov.length; i++) {
-		if (str === randMov[i]){
-			replacer = replacer + str;
-		} else if(randMov[i] === " ") {
-			replacer = replacer + " ";
-		} else {
-			replacer = replacer + "_";
+function letterComp(mov,arr,str) {
+	for (var i = 0; i < mov.length; i++) {
+		if (str === mov[i]){
+			arr.splice(i,1,str);
 		}
-		$('.clues').text(replacer);
+		$('.clues').text(arr.join(''));	
 	}
-}	
+	
+}
+
+/*i = 2 mov.length = 5 {
+	if "i" === l 
+		replacer splice (2), "i";
+	i = 2
+
+}	*/
